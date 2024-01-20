21e0a7eac7b4831d4f1f5fc85cef8bf48148d9f2
Ozodbek <ozodbek@MacBook-Pro-Ozodbek.local>
2024-01-20 11:16:06 +0500

    endi tugadi

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <BrowserRouter>
         <Header/>      
         <Routes>
-          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>}/>
           <Route path="/blog" element={<Blog/>}/>
           <Route path="/contack" element={<Contack/>}/>
