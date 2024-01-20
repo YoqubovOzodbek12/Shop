423fc6e291347f8198ff6e78e21811c9e3040032
Ozodbek <ozodbek@MacBook-Pro-Ozodbek.local>
2024-01-20 11:18:55 +0500

    home error bolib qoldi

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
         <Header/>      
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
           <Route path="/about" element={<About/>}/>
           <Route path="/blog" element={<Blog/>}/>
           <Route path="/contack" element={<Contack/>}/>
